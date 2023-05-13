3046e6c9ad15d2f1e470f751e76c903e6bef4434
eryn510 <76656837+eryn510@users.noreply.github.com>
2023-05-13 19:05:18 +0800

    New Input System, Move, Sprint, Jump

--- Template/Assets/Templates/Scripts/Player/Movement/PlayerMovement.cs
+++ Template/Assets/Templates/Scripts/Player/Movement/PlayerMovement.cs
@@ -1,42 +1,119 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEditor;
+using UnityEditor.Animations;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(CharacterController))]
 public class PlayerMovement : MonoBehaviour
 {
-    public CharacterController controller;
+    [HideInInspector]public CharacterController _controller;
     public Transform cam;
 
-    public float speed = 6f;
+    public float normalSpeed = 6f;
+    public float sprintSpeed = 8f;
+    public float jumpHeight = 4f;
+
+    private float _currentSpeed;
+
+    Animator animator;
+
+    private Vector2 _input;
+    private Vector3 _direction;
+    private Vector3 _moveDir;
+
+    private float _gravity = -9.81f;
+    [SerializeField] private float gravityMultiplier = 1.0f;
+    private float _velocity;
+
+    bool isRunning = false;
 
     public float turnDamp = 0.1f;
     float turnDampVelocity;
 
-    void Start()
+    void Awake()
     {
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
+        animator = this.GetComponent<Animator>();
+        _currentSpeed = normalSpeed;
+        _controller = this.GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
+        applyGravity();
+        applyRotation();
+        applyMovement();
+    }
 
-        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+    void applyGravity()
+    {
+        if (IsGrounded() && _velocity < 0.0f)
+            _velocity = -1.0f;
+        else
+            _velocity += _gravity * gravityMultiplier * Time.deltaTime;
 
-        if(direction.magnitude >= 0.1f)
+        _direction.y = _velocity;
+    }
+
+    void applyRotation()
+    {
+        if (_input.sqrMagnitude == 0)
+            return;
+
+        float targetAngle = Mathf.Atan2(_direction.x, _direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+        float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnDampVelocity, turnDamp);
+        transform.rotation = Quaternion.Euler(0f, angle, 0f);
+        _moveDir = Quaternion.Euler(0f, targetAngle, 0f) * new Vector3(0, _velocity, 1);
+    }
+
+    void applyMovement()
+    {
+        isRunning = false;
+        animator.SetBool("isRunning", isRunning);
+
+        Debug.Log(_currentSpeed);
+
+        if (_input.magnitude >= 0.1f)
         {
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnDampVelocity, turnDamp);
-
-            transform.rotation = Quaternion.Euler(0f, angle, 0f);
-
-            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+            isRunning = true;
+            animator.SetBool("isRunning", isRunning);
         }
 
+
+        _controller.Move(_moveDir.normalized * _currentSpeed * Time.deltaTime);
     }
+
+    public void move(InputAction.CallbackContext context)
+    {
+        _input = context.ReadValue<Vector2>();
+        _direction = new Vector3(_input.x, 0f, _input.y).normalized;
+    }
+
+    public void jump(InputAction.CallbackContext context)
+    {
+        if (!context.started)
+            return;
+        if (!IsGrounded())
+            return;
+
+        _velocity += jumpHeight;
+    }
+
+    public void sprint(InputAction.CallbackContext context)
+    {
+        if (!context.ReadValueAsButton())
+        {
+            _currentSpeed = normalSpeed;
+            return;
+        }   
+        _currentSpeed = sprintSpeed;
+    }
+
+    private bool IsGrounded() => _controller.isGrounded;
 }
